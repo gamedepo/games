23040d167f8a41045875151a7d76b4533c91b5d8
Sean Chen <sean.chen@leocorn.com>
2020-01-22 23:44:41 -0500

    get started form a simple Nuxt.js app

---
+++ layouts/vuetify.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+// Simple layout for Vuetify playground
+v-app
+  v-app-bar(color='indigo' dark fixed app dense)
+    v-app-bar-nav-icon/
+    v-toolbar-title Toolbar Grid Footer
+    v-spacer
+    v-btn(icon)
+      v-icon mdi-magnify
+    v-btn(icon)
+      v-icon mdi-dots-vertical
+
+  v-content
+    Nuxt/
+
+  v-footer(color="indigo" app padless)
+    v-layout(justify-center row wrap)
+      v-flex(py-3 text-center white--text xs12)
+        | &copy;{{new Date().getFullYear()}}- <strong>Leocornus LTD.</strong>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/vuetify/index.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+v-container(grid-list-md text-center)
+  v-layout(row wrap)
+    v-flex(xs12)
+      v-card(dark color="red")
+        v-card-text(class="px-0") {{message}}
+    v-flex(v-for="i in 12 * 20" :key="`b${i}`" xs1)
+      v-card(dark :color="`#${(i * 50).toString(16)}`")
+        v-card-text(class="px-0") {{i}}
+</template>
+
+<script>
+export default {
+  // set the layout, the file name in layouts folder.
+  layout: 'vuetify',
+
+  name: 'VuetifyScratchPad',
+
+  data () {
+    return {
+      message: '',
+    };
+  },
+
+  methods: {
+  },
+
+  created() {
+
+    this.message = "this.$localSettings.applicationName";
+  }
+}
+</script>
