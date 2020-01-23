c22fe6b65d9d33d157d953c851e4d102f866f71d
Sean Chen <sean.chen@leocorn.com>
2020-01-23 12:50:56 -0500

    the simple login page.

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -2,4 +2,25 @@
 div
   h2 Login
   hr
+  v-btn(@click="login") Login Auth0
+
+  v-chip(
+    v-if="$auth.loggedIn"
+    color="primary"
+    v-text="$auth.user"
+  )
 </template>
+
+<script>
+export default {
+    methods: {
+        login: function() {
+
+            return this.$auth.loginWith('auth0')
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }
+}
+</script>
