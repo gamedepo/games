bf50af30db34a9c28e9e7acd3afe2b19294b3ff6
Sean Chen <sean.chen@leocorn.com>
2020-01-22 23:48:45 -0500

    add the missing config.js

---
+++ nuxt.config.js
@@ -0,0 +1,26 @@
+/**
+ * configuration for Nuxt.js
+ */
+export default {
+
+    server: {
+        // port: 3000, // default is 3000
+        host: '0.0.0.0' // default is localhost
+    },
+
+    buildModules: [
+        // load the nuxtjs vutify-module
+        // https://github.com/nuxt-community/vuetify-module
+        '@nuxtjs/vuetify'
+    ],
+
+    /**
+     * options to set up vuetify, details on page
+     *  - https://github.com/nuxt-community/vuetify-module
+     */
+    vuetify: {
+
+        // we could set up default assets here.
+        // By default, automatically handle Roboto font & Material Design Icons
+    }
+}
