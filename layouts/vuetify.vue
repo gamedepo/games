26dc0b4c60670de4bc51ae3da9e3af22d9bf739a
Sean Chen <sean.chen@leocorn.com>
2020-01-27 20:39:11 -0500

    add the profile menu drop down

--- layouts/vuetify.vue
+++ layouts/vuetify.vue
@@ -3,12 +3,29 @@
 v-app
   v-app-bar(color='indigo' dark fixed app dense)
     v-app-bar-nav-icon/
-    v-toolbar-title Toolbar Grid Footer
+    v-toolbar-title Games for fun
     v-spacer
     v-btn(icon)
-      v-icon mdi-magnify
-    v-btn(icon)
-      v-icon mdi-dots-vertical
+      v-icon mdi-home
+    // sign in button for not logged in user.
+    v-btn(
+      v-if="!$auth.loggedIn" 
+      color='indigo'
+      depressed
+      @click="login"
+    )
+      | Sign In
+    v-btn(
+      v-if="$auth.loggedIn" 
+      color="indigo"
+      depressed
+    )
+      v-avatar(
+        size="33"
+      )
+        img(:src="$auth.user.picture")
+        v-icon mdi-menu-down
+
 
   v-content
     Nuxt/
@@ -16,10 +33,19 @@ v-app
   v-footer(color="indigo" app padless)
     v-layout(justify-center row wrap)
       v-flex(py-3 text-center white--text xs12)
-        | &copy;{{new Date().getFullYear()}}- <strong>Leocornus LTD.</strong>
+        | &copy;{{new Date().getFullYear()}} - <strong>Leocornus LTD.</strong>
 </template>
 
 <script>
 export default {
+    methods: {
+        login: function() {
+
+            return this.$auth.loginWith('auth0')
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }
 }
 </script>
